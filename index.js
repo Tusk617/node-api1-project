d22f752f9829284b7e3021a73f0ed83569d51dbf
Tusk617 <59673124+Tusk617@users.noreply.github.com>
2020-05-04 16:14:58 -0400

    Created a post request, and was able to make a post through postman.

--- index.js
+++ index.js
@@ -4,8 +4,30 @@ const server = express();
 
 server.use(express.json());
 
+// creating the users array, which will be called later in a get request
+let users = [
+    {
+        id: 1,
+        name: "CoolGuy7",
+        bio: "Just a cool guy, what can I say?"
+    }
+]
+
 server.get("/", (req, res) => {
     res.json({ api: "Hello World!" });
 })
 
-server.listen(8000, () => console.log("API is up"))
+server.get("/api/users", (req, res) => {
+    //calling the users array
+    res.json(users);
+})
+
+server.post("/api/users", (req, res) => {
+    const userInformation = req.body;
+
+    users.push(userInformation);
+
+    res.status(201).json(userInformation)
+})
+
+server.listen(8000, () => console.log("API is cool"))
