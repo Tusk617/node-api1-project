264b012a0ca9cab0d524ea813aab7774e8ce729e
Tusk617 <59673124+Tusk617@users.noreply.github.com>
2020-05-04 15:24:34 -0400

    Got a basic server set up, it prints hello world

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ const express = require("express");
 const server = express();
 
 server.use(express.json());
+
+server.get("/", (req, res) => {
+    res.json({ api: "Hello World!" });
+})
+
+server.listen(8000, () => console.log("API is up"))
