933c213e832ba417779b225debb615a52451aadd
Tusk617 <59673124+Tusk617@users.noreply.github.com>
2020-06-01 15:45:44 -0400

    Got my get user by id function working, still unsure how to test the 500 error code

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ server.use(express.json())
 
 let users = [
     {
-        id:shortid.generate(),
+        id: 1,
         name: "Andrew W.K",
         bio: "Unnoficial Ambassador of partying"
     }
@@ -20,6 +20,17 @@ server.get("/api/users", (req, res) => {
     if (!users) {
         return res.status(500).json({errorMessage: "The users information could not be retrieved."})
     } else return res.status(200).json(users)
+})
+
+server.get("/api/users/:id", (req, res) => {
+    let userId = Number(req.params.id);
+
+    const filteredUsers = users.filter((user) => user.id === userId);
+    if (filteredUsers[0] === undefined) {
+        return res.status(404).json({error: "User does not exist"})
+    } else if (!users) {
+        return res.status(500).json({error: "User info could not be retrieved"})
+    } else return res.status(200).json(filteredUsers);
 })
 
 //listen for incoming requests
