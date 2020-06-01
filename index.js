c72eebe52b8122d177e7a97f110005124c6ea10f
Tusk617 <59673124+Tusk617@users.noreply.github.com>
2020-06-01 15:21:53 -0400

    Was able to make a user array, give a user a unique id using shortid, then was able to make a get request using postman successfully

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 const express = require("express");
+const shortid = require("shortid");
 
 
 //create a server
 const server = express();
 server.use(express.json())
 
-//functions
+let users = [
+    {
+        id:shortid.generate(),
+        name: "Andrew W.K",
+        bio: "Unnoficial Ambassador of partying"
+    }
+]
 
+//functions
+server.get("/api/users", (req, res) => {
+    res.status(200).json(users)
+})
 
 //listen for incoming requests
 const port = 8000;
